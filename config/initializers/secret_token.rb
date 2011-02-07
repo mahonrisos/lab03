df0e3fc94a6c41bb1ac0599a43a8617538fc4c65
mahonrisos <mahonri_sosa@hotmail.com>
2011-02-07 10:18:54 -0600

    start lab03

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab03::Application.config.secret_token = '1b77f24db4d68538b43eac7f71345e38c212e9062cb984d8435e731d868bb0c299422df2f2acdeb2ea7af5edc1ef9432e5fd997f7842a271cc89705f4050b8e8'
